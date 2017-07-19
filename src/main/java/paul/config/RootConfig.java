726a4e615978c55fbd3c0fc30c9300fa26867d00
Administrator <8406833748@qq.cd>
2017-07-20 00:22:11 +0800

    commit -m"家里蹲"

--- src/main/java/paul/config/RootConfig.java
+++ src/main/java/paul/config/RootConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -18,7 +20,7 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "paul.*", excludeFilters = {@ComponentScan.Filter
         (type = FilterType.ANNOTATION, classes = EnableWebMvc.class)})
 @Configuration
-@MapperScan(value = {"paul.dao"})
+@MapperScan(value ={"paul.dao","resources"})
 public class RootConfig {
     @Bean
     public DataSource dataSource() {
@@ -29,6 +31,9 @@ public class RootConfig {
     public SqlSessionFactoryBean sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
         sqlSessionFactory.setDataSource(dataSource());
+//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        sqlSessionFactory.setMapperLocations(resolver
+//                .getResources("classpath:resources/*.xml"));
         return sqlSessionFactory;
     }
 
